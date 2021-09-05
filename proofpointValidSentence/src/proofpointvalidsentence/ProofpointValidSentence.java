ac87b1d245966d093b04d13d36984421a73de652
ThomasMckeown <47249089+ThomasMckeown@users.noreply.github.com>
2021-09-05 16:01:19 +0100

    Updated part 1,2,3,4. First attempt at part 5
    Added each part into their own class.
    Combined part 2,3,4 together so the sentence is only looped through once

--- proofpointValidSentence/src/proofpointvalidsentence/ProofpointValidSentence.java
+++ proofpointValidSentence/src/proofpointvalidsentence/ProofpointValidSentence.java
@@ -5,6 +5,9 @@
  */
 package proofpointvalidsentence;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author User
@@ -17,85 +20,85 @@ public class ProofpointValidSentence {
      */
     
     
-    public String validSentence(String sentence) {
-        // TODO code application logic here
-        
+    public String characterUpperCase(String sentence) {
         
         //Testing if first character is capital
-        //gets the first character
-        //makes the first character uppercase
-        //compares it to the actual first character to see if they are the same
-        //if they are then the first character is uppercase
-        //trim gets rid of leading or ending spaces
-//        if(Character.isLetter(sentence.trim().charAt(0))
-//        && sentence.charAt(0) == sentence.toUpperCase().charAt(0)){
-//        return "Valid";
-//        }else{
-//        return "Invalid";
-//        }
+        //gets the character at position 0 in the string - trims the sentence to account for any spaces at the start
+        //checks if this character is uppercase - if so returns valid
+        if(Character.isUpperCase(sentence.trim().charAt(0))){
+        return "Valid";
+        }else{
+        return "Invalid";
+        }
         
-//        if(sentence.contains("\"")){
-//            return "Valid";
-//        }else{
-//            return "Invalid";
-//        }
-        
+    }
+    
+    public String amountOfQutationsAndPeriods(String sentence) {
         
         //checking the amount of quotation marks in the string
-//        int count = 0;
-//        //getting length of the string so we know how many times the for loop needs to go to get to the end of the string
-//        for(int i = 0; i < sentence.length(); i++) {
-//            //checking if each charater is a quotatiom mark
-//            if(sentence.charAt(i) == '\"'){
-//                //if it is a count is kept
-//                count++;
-//            }
-//        }
-//        //checking if the remainder when count is divided by 2 is 0
-//        //if it is then this means there is an even amount of quotation marks 
-//        if(count % 2 == 0){
-//            return "Valid";
-//        }else{
-//            return "Invalid";
-//        }
-        
-        //checks if the sentence ends with a period
-//        if(sentence.trim().endsWith(".")){
-//        return "Valid";
-//        }else{
-//        return "Invalid";
-//        }
-
-
-
-        //Making sure there is only one period in the string 
+        int count = 0;
         int count2 = 0;
-        //getting length of the string so we know how many times the for loop needs to go to get to the end of the string
-        for(int i = 0; i < sentence.length(); i++) {
-            //checking if each charater is a period
-            if(sentence.charAt(i) == '.'){
-                //if it is a count is kept
-                count2++;
+        
+        //first checks if the sentence ends with a period. if it doesnt then immediately returns invalid so time isnt wasted going through the sentence looking for
+        //quotation marks and periods
+        if(sentence.trim().endsWith(".")){
+            //getting length of the string so we know how many times the for loop needs to go to get to the end of the string
+            for(int i = 0; i < sentence.length(); i++) {
+                //checking if each charater is a quotatiom mark
+                if(sentence.charAt(i) == '\"'){
+                    //if it is a count is kept
+                    count++;
+                //checking if each character is a period
+                }if(sentence.charAt(i) == '.'){
+                    //if it is a count is kept
+                    count2++;
+                }
             }
-        }
-        //if the count equals 1 it is valid. else if is invalid
-        if(count2 == 1){
+
+        //checking if the remainder when count is divided by 2 is 0
+        //if it is then this means there is an even amount of quotation marks 
+        //also checking if there is only one period. if there is more then one then sentence is invalid so Invalid is returned even if there
+        //is and even amount of quotation marks
+        if(count % 2 == 0 && count2 == 1){
             return "Valid";
         }else{
             return "Invalid";
         }
         
-        
-        
-        
-        
-        
-        
-        
+        //if sentence doesn't end with a period invalid is immediately returned 
+        }else{
+            return "Invalid";
+        }
 
+    }
 
+     public String numbersBelow13AreText(String sentence){
+       //empty sting to concat text to  
+        String result = "";
+        //using regex pattern to look for numbers in the sentence
+        Pattern pattern = Pattern.compile("-?[0-9]+");
+        //using the matcher to match the number found by pattern to an int
+        Matcher matcher = pattern.matcher(sentence);
 
+        while (matcher.find()) {
 
+            //parseing the int found from text to an int so it can be checked
+            int x = Integer.parseInt(matcher.group());
+            //checking if that parsed int is less than 13
+            if(x < 13){
+                result = "Invalid";
+            }else{
+                result = "Valid";
+            }
+            
+        //check in case there is no numbers in the sentence so no match would be found
+        //resulting in the while loop being skipped and result returning an empty string responce
+        }if(result.equals("")){
+            result = result.concat("Valid");
+        }
+            return result;
+
+    
     }
     
 }
--- proofpointValidSentence/test/proofpointvalidsentence/ProofpointValidSentenceTest.java
+++ proofpointValidSentence/test/proofpointvalidsentence/ProofpointValidSentenceTest.java
@@ -31,28 +31,111 @@ public class ProofpointValidSentenceTest {
     //String sentence = "77 The quick brown fox said \"hello Mr lazy dog\". ";
     
     //Valid
-    String sentence = " The quick brown. fox said \"hello Mr lazy dog\". ";
+//    String sentence = " The quick brown fox said \"hello Mr lazy dog\". ";
     
     //Invalid
 //    String sentence2 = "the quick brown fox said \"hello Mr lazy dog\".";
     
     @Test
-    public void testValidSentence1() {
-        System.out.println("Test 1: Valid Sentence");
-        
+    public void testCharacterUpperCase1() {
+        System.out.println("Test 1: Sentence starts with an uppercase letter");
+        String sentence = " The quick brown fox said \"hello Mr lazy dog\". ";
         String expResult = "Valid";
-        String result = instance.validSentence(sentence);
+        String result = instance.characterUpperCase(sentence);
         assertEquals(expResult, result);
     }
-//    
-//    @Test
-//    public void testValidSentence2() {
-//        System.out.println("Test 2: Starting without a capital letter");
-//        
-//        String expResult = "Invalid";
-//        String result = instance.validSentence(sentence2);
-//        assertEquals(expResult, result);
-//    }
+    
+    @Test
+    public void testCharacterUpperCase2() {
+        System.out.println("Test 2: Sentence doesn't start with an uppercase letter");
+        String sentence = " the quick brown fox said \"hello Mr lazy dog\". ";
+        String expResult = "Invalid";
+        String result = instance.characterUpperCase(sentence);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testAmountOfQutationsAndPeriods1() {
+        System.out.println("Test 3: Sentence has an even amount of quotations, only has one period and ends with a period");
+        String sentence = " The quick brown fox said \"hello Mr lazy dog\". ";
+        String expResult = "Valid";
+        String result = instance.amountOfQutationsAndPeriods(sentence);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testAmountOfQutationsAndPeriods2() {
+        System.out.println("Test 4: Sentence has an odd amount of quotations, only has one period and ends with a period");
+        String sentence = " The quick brown fox said hello Mr lazy dog\". ";
+        String expResult = "Invalid";
+        String result = instance.amountOfQutationsAndPeriods(sentence);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testAmountOfQutationsAndPeriods3() {
+        System.out.println("Test 5: Sentence has an even amount of quotations, multiple periods and ends with a period");
+        String sentence = " The quick. brown fox. said \"hello Mr lazy dog\". ";
+        String expResult = "Invalid";
+        String result = instance.amountOfQutationsAndPeriods(sentence);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testAmountOfQutationsAndPeriods4() {
+        System.out.println("Test 6: Sentence has an even amount of quotations, only has one period and doesn't end with a period");
+        String sentence = " The quick brown fox said. \"hello Mr lazy dog\" ";
+        String expResult = "Invalid";
+        String result = instance.amountOfQutationsAndPeriods(sentence);
+        assertEquals(expResult, result);
+    }
+    
+    
+    @Test
+    public void testNumbersBelow13AreText1() {
+        System.out.println("Test 7: One number is in the sentence, 13 or above");
+        String sentence = " The 13 quick brown foxs said \"hello Mr lazy dog\". ";
+        String expResult = "Valid";
+        String result = instance.numbersBelow13AreText(sentence);
+        assertEquals(expResult, result);
+    }
+        
+    @Test
+    public void testNumbersBelow13AreText2() {
+        System.out.println("Test 8: Two numbers are in the sentence, both 13 or above");
+        String sentence = " The 13 quick brown foxes said \"hello Mr lazy dog\", 18 times. ";
+        String expResult = "Valid";
+        String result = instance.numbersBelow13AreText(sentence);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testNumbersBelow13AreText3() {
+        System.out.println("Test 8: Two numbers are in the sentence, one is 13 or above, second is less than 13");
+        String sentence = " The 16 quick brown foxes said \"hello Mr lazy dog\", 11 times. ";
+        String expResult = "Invalid";
+        String result = instance.numbersBelow13AreText(sentence);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testNumbersBelow13AreText4() {
+        System.out.println("Test 8: Two numbers are in the sentence, both are less than 13");
+        String sentence = " The 6 quick brown foxes said \"hello Mr lazy dog\", 11 times. ";
+        String expResult = "Invalid";
+        String result = instance.numbersBelow13AreText(sentence);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testNumbersBelow13AreText5() {
+        System.out.println("Test 8: One number is in the sentence, less than 13");
+        String sentence = " The 6 quick brown foxes said \"hello Mr lazy dog\". ";
+        String expResult = "Invalid";
+        String result = instance.numbersBelow13AreText(sentence);
+        assertEquals(expResult, result);
+    }
+    
     
     
 }
